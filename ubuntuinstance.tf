bafb5eed3813dff5381a467e88360929a76d0a9e
Boby chaudhary <73424060+bobychaudhary@users.noreply.github.com>
2022-04-18 08:40:02 +0530

    Create ubuntuinstance.tf

---
+++ ubuntuinstance.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "server" {
+    count="1"
+    ami="ami-04505e74c0741db8d"
+    instance_type="t2.micro"
+    key_name="mykey"
+    tags {
+        Name="newser"
+        Env="dev"
+    }
+
+}
