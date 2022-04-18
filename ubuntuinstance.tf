3956cc1463bc837a83993e3c374ca6e555934c66
Boby chaudhary <73424060+bobychaudhary@users.noreply.github.com>
2022-04-18 12:46:42 +0530

    Update ubuntuinstance.tf

--- ubuntuinstance.tf
+++ ubuntuinstance.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "server" {
     ami="ami-04505e74c0741db8d"
     instance_type="t2.micro"
     key_name="mykey"
-    tags {
+    tags = {
         Name="newser"
         Env="dev"
     }
