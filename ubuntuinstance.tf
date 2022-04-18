6bf6f3146bbe73f8452cbfd6a49c7cb76fa87634
Boby chaudhary <73424060+bobychaudhary@users.noreply.github.com>
2022-04-18 12:59:54 +0530

    Update ubuntuinstance.tf

--- ubuntuinstance.tf
+++ ubuntuinstance.tf
@@ -1,10 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.74.0"
-    }
-  }
+provider "aws" {
+  region="us-east-1"
 }
 resource "aws_instance" "server" {
     count="1"
