e97eeb25f6086e6ea01ea0fc36e8185017c96e63
Boby chaudhary <73424060+bobychaudhary@users.noreply.github.com>
2022-04-18 12:57:01 +0530

    Update ubuntuinstance.tf

--- ubuntuinstance.tf
+++ ubuntuinstance.tf
@@ -1,7 +1,9 @@
-provider "aws" {
-    region = "us-east-1"
-}
-
+required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.74.0"
+    }
+  }
 resource "aws_instance" "server" {
     count="1"
     ami="ami-04505e74c0741db8d"
